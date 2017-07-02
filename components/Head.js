a11ff7aaac76dc9a36267c37aadbce2063b2cd90
Puxuan He <puxuanhe@gmail.com>
2017-07-01 21:06:21 -0500

    add favicon

--- components/Head.js
+++ components/Head.js
@@ -3,6 +3,9 @@ import Head from 'next/head';
 const _Head = ({ scripts = [] }) => (
   <Head>
     <title>意栈网</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="96x96" href="/static/favicon-96x96.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,400,500" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
